9c022d98b3e0ee3cb427422add6156a263891995
Tim Wang <tim@primecall.com.au>
2015-06-23 15:57:51 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pininteresting::Application.config.secret_key_base = 'ab59d3e29849faab0ceb9b44b4fafa58225e72c7d5c0c5550e589e8761466a97dc20afc7166e304e7399efdbcb9dc560016b9d59b762e46b4385c856914a4b1c'
